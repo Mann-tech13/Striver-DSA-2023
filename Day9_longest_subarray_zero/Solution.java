4c09ae8ef9aab6a85f7999335fb838e65888c0ac
Mann-tech13 <mannshah.ict19@gmail.com>
2023-06-14 23:55:11 +0530

    Day9 -> 1 problem

---
+++ Day9_longest_subarray_zero/Solution.java
@@ -0,0 +1,30 @@
+package Day9_longest_subarray_zero;
+
+import java.util.* ;
+
+import java.util.ArrayList;
+
+public class Solution {
+
+	public static int LongestSubsetWithZeroSum(ArrayList<Integer> arr) {
+		int maximum = 0;
+		int sum = 0;
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for(int i=0; i<arr.size(); i++){
+			sum += arr.get(i);
+			if(sum == 0){
+				maximum = i+1;
+			}
+			else{
+				if(map.get(sum) != null) {
+					maximum = Math.max(maximum, i - map.get(sum)); 
+				}
+				else {
+					map.put(sum, i); 
+				}
+			}
+		}
+
+		return maximum;
+	}
+}
