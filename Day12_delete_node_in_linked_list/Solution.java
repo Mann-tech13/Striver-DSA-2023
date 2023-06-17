fb209554b9fb1375a1c1ac2ba42f6997229a74e1
Mann-tech13 <mannshah.ict19@gmail.com>
2023-06-17 12:15:59 +0530

    Day12 -> 3* problems

---
+++ Day12_delete_node_in_linked_list/Solution.java
@@ -0,0 +1,18 @@
+package Day12_delete_node_in_linked_list;
+
+class LinkedListNode<T> {
+    T data;
+    LinkedListNode<T> next;
+
+    LinkedListNode(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Solution {
+    public static void deleteNode(LinkedListNode<Integer> node) {
+        node.data = node.next.data;
+        node.next = node.next.next;
+    }
+}
