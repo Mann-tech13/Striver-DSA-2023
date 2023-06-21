4db2ab017c39b76e343f6edec7b92b186bdb3623
Mann-tech13 <mannshah.ict19@gmail.com>
2023-06-21 23:56:00 +0530

    Day16 -> 3 problems

---
+++ Day16_preorder_traversal/Solution.java
@@ -0,0 +1,38 @@
+package Day16_preorder_traversal;
+
+import java.util.*;
+
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.data = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.data = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public static List<Integer> getPreOrderTraversal(TreeNode root) {
+        return treeTraversal(root, new ArrayList<Integer>());
+    }
+
+    public static List<Integer> treeTraversal(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return res;
+        }
+        res.add(root.data);
+        res = treeTraversal(root.left, res);
+        return treeTraversal(root.right, res);
+
+    }
+}
